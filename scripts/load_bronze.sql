ffaf9f4ab90566ef36345576bd0f78c1a7775f2d
Nanda Kumar <140781575+Nanda-NK17@users.noreply.github.com>
2025-06-24 15:29:17 +0530

    Update load_bronze.sql

--- scripts/load_bronze.sql
+++ scripts/load_bronze.sql
@@ -6,3 +6,47 @@ BULK INSERT bronze.crm_cust_info
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
+
+BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Temp\source_crm.csv\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Temp\source_crm.csv\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Temp\Data sets Datawarehouse project\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Temp\Data sets Datawarehouse project\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Temp\Data sets Datawarehouse project\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
